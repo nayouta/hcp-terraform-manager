1a472012872aecc50fed0e8b65a06db560c21792
Nayouta <rotus7.dev@gmail.com>
2025-03-07 23:05:34 +0900

    create import block

---
+++ provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  cloud {
+    organization = "nayouta"
+    hostname     = "app.terraform.io"
+    workspaces {
+      name = "hcp-terraform-manager"
+    }
+  }
+}
+
+provider "tfe" {
+  hostname = "app.terraform.io" # HCP Terraformのエンドポイント
+  token    = var.tfe_token
+}
+
