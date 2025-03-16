b24a8f9e0edfb8172035c31fc51c781312429b58
Nayouta <rotus7.dev@gmail.com>
2025-03-16 23:28:26 +0900

    change vcs config

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 module "workspace_manager" {
-  source                     = "./modules/workspace_manager"
-  workspace_name             = var.workspace_name
-  workspace_description      = var.workspace_description
-  organization               = var.organization_name
-  vcs_repo_identifier        = var.vcs_repo_identifier
-  vcs_repo_branch            = var.vcs_repo_branch
-  github_app_installation_id = var.github_app_installation_id
+  source                = "./modules/workspace_manager"
+  workspace_name        = var.workspace_name
+  workspace_description = var.workspace_description
+  organization          = var.organization_name
+  vcs_repo_identifier   = var.vcs_repo_identifier
+  vcs_repo_branch       = var.vcs_repo_branch
+  oauth_token_id        = var.oauth_token_id
 }
